eeea828dea2d3771a4a48c15b113d921edb49971
Ka Ho Lam <andylam1018@gmail.com>
2021-08-28 01:08:07 -0400

    add progress bar and option to output combined frames in npz

--- combine_frames.py
+++ combine_frames.py
@@ -2,6 +2,7 @@ import argparse
 from collections import defaultdict
 
 import numpy as np
+from tqdm import tqdm
 
 from frame import Frame
 from utils import serialize
@@ -10,54 +11,68 @@ from utils import serialize
 def main():
     parser = argparse.ArgumentParser(description='Combine output from particles per frame to history of particles. '
                                                  'Also extract density, velocity and temperature information.')
-    parser.add_argument('frames', action='store', nargs='+', type=str,
+    parser.add_argument('--frames', action='store', nargs='+', type=str,
                         help='primitives variables in athdf format')
-    parser.add_argument('-f', '--format', action='store', type=str, default='particle.%05d.npy',
-                        help='output filename format containing one format specifier')
+    parser.add_argument('-y', '--npy', action='store', type=str,
+                        help='output in npy format and set the filename format (with one format specifier)')
+    parser.add_argument('-z', '--npz', action='store', type=str, default='particles.npz',
+                        help='output in npz format and set the filename')
+    parser.add_argument('--ncols', action='store', type=int,
+                        help='number of columns used to print progress bar')
     args = parser.parse_args()
 
     # construct a sorted list of frames
-    frames = [Frame(filename) for filename in args.frames]
+    with tqdm(args.frames, ncols=args.ncols,
+              bar_format='{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}] {desc}') as t:
+        frames = []
+        for filename in t:
+            t.set_description_str(f'Reading header from {filename}')
+            frames.append(Frame(filename))
     frames.sort()
 
     mesh2mb_cache = dict()
     interpcc_cache = dict()
     record = defaultdict(list)
 
-    for frame in frames:
-        print('Working on frame', frame.filename)
-        h = dict(frame.header)
-        for key in ['Time', 'NumCycles']:
-            del h[key]
+    with tqdm(frames, ncols=args.ncols,
+              bar_format='{percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}] {desc}') as t:
+        for frame in t:
+            t.set_description_str(f'Working on {filename}')
+            h = dict(frame.header)
+            for key in ['Time', 'NumCycles']:
+                del h[key]
 
-        hash_key = serialize(h)
-        if hash_key not in mesh2mb_cache:
-            mesh2mb_cache[hash_key] = frame.mesh_position_to_meshblock_id
-        if hash_key not in interpcc_cache:
-            interpcc_cache[hash_key] = frame.interpolate_cell_centered
+            hash_key = serialize(h)
+            if hash_key not in mesh2mb_cache:
+                mesh2mb_cache[hash_key] = frame.mesh_position_to_meshblock_id
+            if hash_key not in interpcc_cache:
+                interpcc_cache[hash_key] = frame.interpolate_cell_centered
 
-        mesh2mb = mesh2mb_cache[hash_key]
-        interpcc = interpcc_cache[hash_key]
+            mesh2mb = mesh2mb_cache[hash_key]
+            interpcc = interpcc_cache[hash_key]
 
-        par = np.load(frame.filename + '.npz')
-        time = par['time'][()]
-        pids = par['pids']
-        meshs = par['meshs']
+            par = np.load(frame.filename + '.npz')
+            time = par['time'][()]
+            pids = par['pids']
+            meshs = par['meshs']
 
-        frame.load(['rho', 'vel1', 'vel2', 'vel3', 'press'])
+            frame.load(['rho', 'vel1', 'vel2', 'vel3', 'press'])
 
-        quantities = np.stack([frame.data['vel1'], frame.data['vel2'], frame.data['vel3'],
-                               frame.data['rho'], frame.data['press'] / frame.data['rho']])
+            quantities = np.stack([frame.data['vel1'], frame.data['vel2'], frame.data['vel3'],
+                                   frame.data['rho'], frame.data['press'] / frame.data['rho']])
 
-        for pid, (x1, x2, x3) in zip(pids, meshs):
-            mb = mesh2mb(x1, x2, x3)
-            q = interpcc(quantities, mb, x1, x2, x3)
-            record[pid].append(np.concatenate([[time, x1, x2, x3], q]))
+            for pid, (x1, x2, x3) in zip(pids, meshs):
+                mb = mesh2mb(x1, x2, x3)
+                q = interpcc(quantities, mb, x1, x2, x3)
+                record[pid].append(np.concatenate([[time, x1, x2, x3], q]))
 
-        frame.unload()
+            frame.unload()
 
-    for pid in record:
-        np.save(args.format % pid, record[pid])
+    if args.npy is not None:
+        for pid in record:
+            np.save(args.npy % pid, record[pid])
+    else:
+        np.savez(args.npz, **{str(k): np.array(v) for k, v in record.items()})
 
 
 if __name__ == '__main__':
